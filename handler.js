0b525ca75962b8b2997ccb4d685c79022f557b56
Felix RP <felix.rp@financial-cloud.com>
2018-12-07 09:38:00 +0000

    Added complex task.

--- handler.js
+++ handler.js
@@ -1,17 +1,22 @@
 'use strict';
 
-module.exports.endpoint = (event, context, callback) => {
+module.exports.endpoint = async (event, context) => {
   // Event in string format?
   const eventParsed = typeof event === "string" ? JSON.parse(event) : event;
   // Welcome message
   const msg = eventParsed.msg || 'all';
 
+  // Imagine that this is a particularly complex or long code that could by nice to have an unit test.
+  const complexResult = 21 + 21;
+  // End of complex or long text.
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Hello ${msg}, the current time is ${new Date().toTimeString()}.`,
+      message: `Hello ${msg}, the current time is ${new Date().toTimeString()}.
+      The complex result is ${complexResult}`,
     }),
   };
 
-  callback(null, response);
+  return response;
 };
