47a65fbba749fe03294f0832d98820c60139bb51
Felix RP <felix.rp@financial-cloud.com>
2018-12-07 10:53:48 +0000

    Updated complexTask test.

--- handler.js
+++ handler.js
@@ -1,8 +1,12 @@
 'use strict';
 
 async function complexTask(data) {
+  const param = parseInt(data) || 0
+  if (data === 23) {
+    return Promise.reject('error ' + data);
+  }
   // Imagine that this is a particularly complex or long code that could by nice to have an unit test.
-  const task = 21 + 21 + data;
+  const task = 21 + 21 + param;
   // End of complex or long text.
   return task;
 }
--- test/complex.test.js
+++ test/complex.test.js
@@ -2,32 +2,40 @@ require('dotenv').config();
 const chai = require('chai');
 const { complexTask } = require('../handler');
 const EVENT = require('./event.json');
-const eventBody = 5 || JSON.parse(EVENT.Records[0].body);
+const eventData = EVENT.number;
+const envData = process.env.ENV_VAR;
 
 describe("Example of complex function from a lambda function unit test.", () => {
-    it("It shold be 42", async () => {
+    it("It should be 42", async () => {
         let result;
         try {
-            result = await complexTask(eventBody);
+            result = await complexTask();
         } catch (err) {
-            chai.expect(err).to.deep.equal(true);
+            chai.expect(err).to.deep.equal('error ' + err);
             return;
         }
-        console.log(result);
-        debugger;
-        chai.expect(result).to.deep.equal(true);
+        chai.expect(result).to.equal(42);
         return;
     });
-    it("It shold catch error", async () => {
+    it("It should be 56", async () => {
         let result;
         try {
-            result = await complexTask(eventBody);
+            result = await complexTask(eventData);
         } catch (err) {
-            chai.expect(err).to.deep.equal(true);
+            chai.expect(err).to.deep.equal('error ' + err);
             return;
         }
-        console.log(result);
-        debugger;
+        chai.expect(result).to.deep.equal(56);
+        return;
+    });
+    it("It should catch error", async () => {
+        let result;
+        try {
+            result = await complexTask(envData);
+        } catch (err) {
+            chai.expect(err).to.deep.equal('error 23');
+            return;
+        }
         chai.expect(result).to.deep.equal(true);
         return;
     });
